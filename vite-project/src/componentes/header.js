926f387f5aa612d826c41e15b0b3b1d1d38cff82
Ramón López <lopezzunigaramon@fpllefia.com>
2024-02-16 15:13:32 +0100

    Update Header

--- vite-project/src/componentes/header.js
+++ vite-project/src/componentes/header.js
@@ -89,6 +89,18 @@ export const header = {
           // menú usuario: No tiene
           break
       }
+      
+      try {
+        // email y rol
+        document.querySelector('#emailUserMenu').innerHTML = ls.getUsuario().email
+        document.querySelector('#rolUserMenu').innerHTML = ls.getUsuario().rol
+        // para la imagen de avatar (avatar.png si el campo está vacío)
+        const imagen = ls.getUsuario().avatar === '' ? 'images/avatar.svg' : ls.getUsuario().avatar
+        document.querySelector('#avatarMenu').setAttribute('src', imagen)
+      } catch (error) {
+        console.log('El usuario no está registrado y no tiene menú de usuario');
+      }
+      
     }
 
   }
