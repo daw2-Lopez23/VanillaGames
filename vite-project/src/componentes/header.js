2f5ab7fdf30589265d7039fcc7b5f5b8de56e63e
Ramón López <lopezzunigaramon@fpllefia.com>
2024-02-16 15:21:12 +0100

    CerrarSesion

--- vite-project/src/componentes/header.js
+++ vite-project/src/componentes/header.js
@@ -2,12 +2,13 @@
 import { ls } from '../componentes/funciones'
 import { menuRol, menuUsuario } from './menus'
 import { editarPerfil } from './editarPerfil'
+
 export const header = {
-    template: // html
-    `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  template: // html
+  `
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
-    <a class="navbar-brand" href="#/home"
+    <a class="navbar-brand router-link" href="#/home"
       ><img
         src="images/logo.svg"
         alt=""
@@ -15,6 +16,7 @@ export const header = {
         height="24"
         class="d-inline-block align-text-top"
       />
+
       Vanilla Games</a
     >
     <button
@@ -29,80 +31,85 @@ export const header = {
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
-      <!-- Menu común para todos los usuarios -->
       <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#/home">Home</a>
+          <a class="nav-link active router-link" aria-current="page" href="#/home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
+          <a class="nav-link router-link" aria-current="page" href="#">TOP5 Proyectos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">A cerca de</a>
+          <a class="nav-link router-link" aria-current="page" href="#" class="router-link">A cerca de</a>
         </li>
       </ul>
 
-      <!-- Aquí va el Menu rol -->
       <div id="menuRol"></div>
-    
-      <!-- Aquí va el Menu usuario -->
       <div id="menuUsuario"></div>
-    
-    </div>
-    <div id="modal">
     </div>
   </div>
 </nav>
-  
-    `,
-    // importamos la función ls del archivo funciones
+<div id="modal"></div>
 
-  
-    script: () => {
-      console.log('Header cargado')
-      document.querySelector('#modal').innerHTML = editarPerfil.template
-      const rolUsuario = ls.getUsuario().rol
-  
-      switch (rolUsuario) {
-        case 'registrado':
-          // menú rol
-          document.querySelector('#menuRol').innerHTML = menuRol.templateRegistrado
-          // menú usuario
-          document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateRegistrado
-          break
-        case 'desarrollador':
-          // menú rol
-          document.querySelector('#menuRol').innerHTML = menuRol.templateDesarrollador
-          // menú usuario
-          document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateDesarrollador
-          break
-        case 'admin':
-          // menú rol
-          document.querySelector('#menuRol').innerHTML = menuRol.templateAdmin
-          // menú usuario
-          document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateAdmin
-          break
-        default : // Para usuarios anónimos
-          // menú rol
-          document.querySelector('#menuRol').innerHTML = menuRol.templateAnonimo
-          // menú usuario: No tiene
-          break
-      }
-      
-      try {
-        // email y rol
-        document.querySelector('#emailUserMenu').innerHTML = ls.getUsuario().email
-        document.querySelector('#rolUserMenu').innerHTML = ls.getUsuario().rol
-        // para la imagen de avatar (avatar.png si el campo está vacío)
-        const imagen = ls.getUsuario().avatar === '' ? 'images/avatar.svg' : ls.getUsuario().avatar
-        document.querySelector('#avatarMenu').setAttribute('src', imagen)
-      } catch (error) {
-        console.log('El usuario no está registrado y no tiene menú de usuario');
-      }
-      
+  `,
+  script: () => {
+    console.log('Header cargado')
+    // Cargamos la ventana modal para editar perfil
+    document.querySelector('#modal').innerHTML = editarPerfil.template
+    // Y ejecutamos su lógica
+    editarPerfil.script()
+    const rolUsuario = ls.getUsuario().rol
+    switch (rolUsuario) {
+      case 'registrado':
+        // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateRegistrado
+        // menú usuario
+        document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateRegistrado
+        break
+      case 'desarrollador':
+        // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateDesarrollador
+        // menú usuario
+        document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateDesarrollador
+        break
+      case 'admin':
+        // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateAdmin
+        // menú usuario
+        document.querySelector('#menuUsuario').innerHTML = menuUsuario.templateAdmin
+        break
+      default : // Para usuarios anónimos
+        // menú rol
+        document.querySelector('#menuRol').innerHTML = menuRol.templateAnonimo
+        // menú usuario - No debe aparecer nada
+        document.querySelector('#menuUsuario').innerHTML = ''
+        break
     }
 
+    // Y actualizamos los datos de menu de usuario si es que se está mostrando
+    try {
+      // email y rol
+      document.querySelector('#emailUserMenu').innerHTML = ls.getUsuario().email
+      document.querySelector('#rolUserMenu').innerHTML = ls.getUsuario().rol
+      // para la imagen de avatar (avatar.png si el campo está vacío)
+      const imagen = ls.getUsuario().avatar === '' ? 'images/avatar.svg' : ls.getUsuario().avatar
+      document.querySelector('#avatarMenu').setAttribute('src', imagen)
+    } catch (error) {
+      console.log('El usuario no está registrado y no tiene menú de usuario')
+    }
+
+    // Cerrar sesión
+    // Capturamos clic sobre el item de cerrar sesión
+    document.querySelector('header').addEventListener('click', (e) => {
+      if (e.target.classList.contains('cerrarSesion')) {
+        e.preventDefault()
+        // Borramos el localstorage
+        ls.setUsuario('')
+        // Cargamos la pagina home
+        window.location = '#/home'
+        header.script()
+      }
+    })
   }
-  
+}
+
   
